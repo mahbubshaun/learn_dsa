0922cee5dbe3a68bbe781f2e4792105e2e3e7273
mahbubshaun <mdmahbubshaun@gmail.com>
2024-11-05 23:48:45 +0600

    practice

--- array/practice.js
+++ array/practice.js
@@ -1,4 +1,23 @@
-let arr = [1,2,3]
-console.log(arr);
-for (x of arr)
-    console.log(x)
+
+
+function findMax(arr)
+{
+    let MAX = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > MAX) {
+            MAX = arr[i]
+
+        }
+    }
+
+    return MAX;
+
+}
+
+
+
+
+let arr = [1,300, 6,3, 110, 20]
+console.log(findMax(arr));
+// for (x of arr)
+//     console.log(x)
