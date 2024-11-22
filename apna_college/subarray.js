1d6d5f217367e150fb79f8b07f1a2f5ac4ba7d6c
mahbubshaun <mdmahbubshaun@gmail.com>
2024-11-22 20:16:14 +0600

    prefix sum

--- apna_college/subarray.js
+++ apna_college/subarray.js
@@ -1,19 +1,42 @@
-const numbers = [2, 4, 6, 8, 10];
+const numbers = [1, -2, 6, -1, 3];
 printSubArray(numbers);
 function printSubArray(numbers)
 {
     let total = 0;
+    let minimum_sum = Number.POSITIVE_INFINITY;
+    let maximum_sum = Number.NEGATIVE_INFINITY;
+    const prefix = new Array(numbers.length);
+    prefix[0] = numbers[0];
+
+    for(let n=1;n<prefix.length;n++)
+    {
+        prefix[n] = prefix[n-1]+ numbers[n];
+    }
     for(let i=0;i<numbers.length;i++)
     {
         for(let j=i;j<numbers.length;j++)
         {
-            for(let k=i;k<=j;k++)
+            let current_sum = 0;
+            // for(let k=i;k<=j;k++)
+            // {
+            //     console.log(numbers[k] + " ");
+            //     current_sum = current_sum+numbers[k];
+
+            // }
+            current_sum = i == 0 ? prefix[j] : prefix[j] - prefix[i-1];
+            total++;
+            if(current_sum > maximum_sum)
             {
-                console.log(numbers[k] + " ");
-                
+                maximum_sum = current_sum;
 
             }
-            total++;
+            if (maximum_sum < minimum_sum)
+            {
+                minimum_sum = maximum_sum;
+
+            }
+            console.log('maximum: ', maximum_sum);
+            console.log('minimum sum: ', minimum_sum);
 
             console.log("\n");
 
@@ -21,4 +44,6 @@ function printSubArray(numbers)
         console.log("\n");
     }
     console.log('total subarrays: ', total);
+    console.log('maximum: ', maximum_sum);
+    console.log('minimum sum: ', minimum_sum);
 }
